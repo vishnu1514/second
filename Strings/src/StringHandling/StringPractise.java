93c20e6be0dc35cbf4185cefb2711173170d0335
vishn <vishn@192.168.0.103>
2022-05-17 00:42:27 +0530

    bbb

---
+++ Strings/src/StringHandling/StringPractise.java
@@ -0,0 +1,20 @@
+package StringHandling;
+
+public class StringPractise {
+
+	public static void main(String[] args) {
+		
+		public class Test{    
+			Long a; //Line 1
+			long b;
+			public Test(long c) {
+			 b = a+c; //Line 2
+			 System.out.println(b);
+			     }
+			     public static void main(String[] args) {
+			    new Test(new Long(10L));
+			     }
+
+	}
+
+}
